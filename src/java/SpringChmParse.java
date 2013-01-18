42af811baa7bdefd3ff4f6b98a8f47b6bdc6f5b6
wjw465150 <wjw465150@gmail.com>
2013-01-18 23:43:17 +0800

    @wjw_fix: 完全重构,并重新采用非递归方式实现.

--- src/java/SpringChmParse.java
+++ src/java/SpringChmParse.java
@@ -1,67 +1,48 @@
 import java.io.File;
-import java.io.IOException;
+import java.util.LinkedList;
 
 import jodd.io.FileUtil;
 import jodd.jerry.Jerry;
-import jodd.jerry.JerryFunction;
 import jodd.lagarto.dom.Node;
 
+//生成Spring3参考指南的目录数据
 public class SpringChmParse {
   static final String CF = "\r\n";
-  static final StringBuilder sb = new StringBuilder(1 * 1024 * 1024);
-  static int _level = 0;
-  static int _pos = 0;
 
-  public static void main(String[] args) throws IOException {
-    File file = new File("Spring3.2Catalog.html");
+  private class WrapperNodeLevel {
+    final Node node;
+    final int level;
+
+    WrapperNodeLevel(Node node, int level) {
+      this.node = node;
+      this.level = level;
+    }
+
+  }
+
+  private void parse() throws Exception {
+    File file = new File("Spring3.XCatalog.html");
+    StringBuilder sb = new StringBuilder(1 * 1024 * 1024);
 
     long startTime = System.currentTimeMillis();
     Jerry doc = Jerry.jerry(FileUtil.readString(file, "gb2312"));
 
-    // parse
-    doc.$("div>dl>dt").each(new JerryFunction() {
-      public boolean onNode(Jerry $this, int index) {
-        String title = $this.$("a").text().replaceAll("(\r\n|\r|\n|\n\r)", "");
-        String href = $this.$("a").attr("href");
-        sb.append("TitleList.Title." + _pos + "=" + title).append(CF);
-        sb.append("TitleList.Level." + _pos + "=" + _level).append(CF);
-        sb.append("TitleList.Url." + _pos + "=" + href).append(CF);
-        sb.append("TitleList.Icon." + _pos + "=" + "0").append(CF);
-        sb.append("TitleList.Status." + _pos + "=" + "0").append(CF);
-        sb.append("TitleList.Keywords." + _pos + "=" + "").append(CF);
-        sb.append("TitleList.ContextNumber." + _pos + "=" + "999" + _pos).append(CF);
-        sb.append("TitleList.ApplyTemp." + _pos + "=" + "0").append(CF);
-        sb.append("TitleList.Expanded." + _pos + "=" + "0").append(CF);
-        sb.append("TitleList.Kind." + _pos + "=" + "0").append(CF);
+    Node[] roots = doc.$("div>dl>dt").get();
 
-        Jerry jerryDD = $this.next();
-        if (jerryDD.get(0) != null && jerryDD.get(0).getNodeName().equals("dd")) {
-          _pos = enumDD(jerryDD.get(0), _level + 1, _pos);
-        }
-        return true;
-      }
-    });
+    int pos = 0;
+    LinkedList<WrapperNodeLevel> list = new LinkedList<WrapperNodeLevel>();
+    for (Node child : roots) { // 1.先添加第1层的子节点到迭代列表里
+      list.add(new WrapperNodeLevel(child, 0));
+    }
 
-    sb.insert(0, "TitleList=" + (_pos + 1) + CF);
-    sb.append(CF);
-    FileUtil.writeString("xxx.txt", sb.toString());
+    WrapperNodeLevel wrap;
+    while (!list.isEmpty()) { // 2. 开始迭代
+      wrap = list.removeFirst(); // 移除并返回此列表的第一个元素
 
-    System.out.println("use time:" + (System.currentTimeMillis() - startTime) + "毫秒!");
-  }
-
-  static int enumDD(Node nodeDD, int level, int pos) {
-    Node[] dldts = nodeDD.getChildElement(0).getChildElements();
-    for (Node dldt : dldts) {
-      if (!dldt.getNodeName().equals("dt")) {
-        continue;
-      }
-
-      pos++;
-
-      String title = dldt.getTextContent().replaceAll("(\r\n|\r|\n|\n\r)", "");
-      String href = dldt.getFirstChild().getFirstChild().getAttribute("href");
+      String title = wrap.node.getTextContent().replaceAll("(\r\n|\r|\n|\n\r)", "");
+      String href = wrap.node.getFirstChild().getFirstChild().getAttribute("href");
       sb.append("TitleList.Title." + pos + "=" + title).append(CF);
-      sb.append("TitleList.Level." + pos + "=" + level).append(CF);
+      sb.append("TitleList.Level." + pos + "=" + wrap.level).append(CF);
       sb.append("TitleList.Url." + pos + "=" + href).append(CF);
       sb.append("TitleList.Icon." + pos + "=" + "0").append(CF);
       sb.append("TitleList.Status." + pos + "=" + "0").append(CF);
@@ -70,13 +51,32 @@ public class SpringChmParse {
       sb.append("TitleList.ApplyTemp." + pos + "=" + "0").append(CF);
       sb.append("TitleList.Expanded." + pos + "=" + "0").append(CF);
       sb.append("TitleList.Kind." + pos + "=" + "0").append(CF);
+      pos++;
 
-      Node nextSibling = dldt.getNextSibling();
+      Node nextSibling = wrap.node.getNextSibling();
       if (nextSibling != null && nextSibling.getNodeName().equals("dd")) {
-        pos = enumDD(nextSibling, level + 1, pos);
+        Node[] dldts = nextSibling.getChildElement(0).getChildElements();
+        int insertPos = 0;
+        for (Node dldt : dldts) {
+          if (!dldt.getNodeName().equals("dt")) {
+            continue;
+          }
+
+          list.add(insertPos++, new WrapperNodeLevel(dldt, wrap.level + 1)); // 3.有子节点则加入迭代列表
+        }
       }
     }
 
-    return pos;
+    sb.insert(0, "TitleList=" + pos + CF);
+    FileUtil.writeString("Spring3.XCatalog.txt", sb.toString());
+
+    System.out.println("use time:" + (System.currentTimeMillis() - startTime) + "毫秒!");
+
   }
+
+  public static void main(String[] args) throws Exception {
+    SpringChmParse springChmParse = new SpringChmParse();
+    springChmParse.parse();
+  }
+
 }
